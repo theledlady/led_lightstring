608e85ecec60ee17e32320511a89510b69db94ba
Paula Shedden <64016410+theledlady@users.noreply.github.com>
2020-08-07 13:32:02 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+let strip = neopixel.create(DigitalPin.P2, 8, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.Red))
+basic.pause(1000)
+strip.clear()
+strip.showColor(neopixel.colors(NeoPixelColors.Green))
+basic.pause(1000)
+strip.clear()
+strip.showColor(neopixel.colors(NeoPixelColors.Blue))
